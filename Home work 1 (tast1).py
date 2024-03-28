06f14e466b512fc2ff120b4df4bcb6ffd35f6923
Yurii Yevpak <132289443+YuriiYevpak@users.noreply.github.com>
2024-03-28 11:07:45 +0200

    Add files via upload

---
+++ Home work 1 (tast1).py
@@ -0,0 +1,13 @@
+# #Task 1
+
+def main():
+
+    name = input("What is your name? ")
+    age = input("How old are you? ")
+    city = input("Where do you live? ")
+
+    print(f"Hello, {name}!")
+    print(f"Your age is {age}.")
+    print(f"Your city is {city}.")
+if __name__ == "__main__":
+    main()
---
+++ Home work 1 (tast2).py
@@ -0,0 +1,28 @@
+
+#Task 2
+
+
+def main():
+    a = float(input("Enter first number(A): "))
+    b = float(input("Enter second number(B): "))
+    sum_result = a + b
+    diff_result = a - b
+    product_result = a * b
+    quotient_result = a / b
+    power_result = a ** b
+    floor_div_result = a // b
+    mod_result = a % b
+    print(f"A + B = {sum_result}")
+    print(f"A - B = {diff_result}")
+    print(f"A * B = {product_result}")
+    print(f"A / B = {quotient_result}")
+    print(f"A ** B = {power_result}")
+    print(f"A // B = {floor_div_result}")
+    print(f"A % B = {mod_result}")
+if __name__ == "__main__":
+    main()
+
+"""
+print
+"""
+/*print
